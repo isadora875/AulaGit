e55494eba2996037a67852c2010b91c608598e16
Isadora <isadoralvesramos6@gmail.com>
2025-04-07 15:43:32 -0300

    Testes no código

--- git.js
+++ git.js
@@ -3,5 +3,6 @@ console.log(`lista de todas as comidas ${comidas}`);
 console.log(`primeira comida ${comidas[0]}`);
 comidas.sort();
 console.log(`Comidas ordenadas: ${comidas}`);
+console.log('FIM');
 
 
